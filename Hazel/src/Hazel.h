044ecb3a001657b245379a537309b08a183d8740
PerhapsChen <451622433@qq.com>
2023-05-28 23:13:09 +0800

    add spdlog.

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -2,8 +2,10 @@
 
 // For use by Hazel application
 
-#include <stdio.h>
+//#include <stdio.h>
+
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // ---- Entry Point --------------------
 #include "Hazel/EntryPoint.h"
